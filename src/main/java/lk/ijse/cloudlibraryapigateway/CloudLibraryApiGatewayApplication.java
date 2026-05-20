dd434733fcf47ad4a3720640610d667b2f0e0e9c
praween-imalsha <praweendissanayake09@gmail.com>
2026-05-20 20:49:20 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/cloudlibraryapigateway/CloudLibraryApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cloudlibraryapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudLibraryApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudLibraryApiGatewayApplication.class, args);
+    }
+
+}
